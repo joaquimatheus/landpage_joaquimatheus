00b10f8c42b2da08d0f1c081383a521db95ee8d9
joaquimatheus <matheus@marcogabriel.com.br>
2022-04-13 18:53:49 -0300

    config the knexfile on more time and upgrade the package.json

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,23 @@
 require('./dotenv');
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
-    client: 'pg'
+  development: {
+    client: 'postgresql',
     connection: {
-        host: process.env.PSQL_HOST,
-        user: process.env.PSQL_USER,
-        database: process.env.PSQL_DATABASE,
-        password: process.env.PSQL_PASSWORD
+      host: process.env.PSQL_DB_HOST,
+      database: process.env.PSQL_DB_DATABASE,
+      user:     process.env.PSQL_DB_USERNAME,
+      password: process.env.PSQL_DB_PASSWORD
     },
-    pool: { min: 2, max: 10 }
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
 };
