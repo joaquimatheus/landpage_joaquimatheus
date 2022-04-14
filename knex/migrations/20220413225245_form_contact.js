465bd233dbdbf8bf499ac4c7e08be228e3148d9c
joaquimatheus <matheus@marcogabriel.com.br>
2022-04-14 20:44:09 -0300

    create table users, with name, email, msg and id

--- knex/migrations/20220413225245_form_contact.js
+++ knex/migrations/20220413225245_form_contact.js
@@ -1,7 +1,24 @@
 exports.up = function(knex) {
-  
+    return knex.raw(`
+        CREATE SEQUENCE seq_users;
+        CREATE TABLE users (
+            id INT NOT NULL
+                CONSTRAINT pk_users PRIMARY KEY DEFAULT NEXTVAL('seq_users'),
+
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            msg TEXT NOT NULL,
+
+            utc_created_on TIMESTAMP
+                NOT NULL CONSTRAINT df_users_utc_created_on DEFAULT (NOW())
+        );
+
+        ALTER SEQUENCE seq_users OWNED BY users.id;
+    `)  
 };
 
 exports.down = function(knex) {
-  
+    return knex.raw(`
+        DROP TABLE users;
+    `) 
 };
