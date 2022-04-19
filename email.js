f6feb4ec1cd0f54ef08c9161b358e5ff8af36545
matheus <matheus@marcogabriel.com.br>
2022-04-19 20:03:31 -0300

    Email sender minimal works;

--- email.js
+++ email.js
@@ -3,22 +3,25 @@ const nodemailer = require('nodemailer');
 
 async function sendEmail() {
     const transporter = nodemailer.createTransport({
-        host: "",
+        host: process.env.AWS_SES_HOST,
         port: 587,
         secure: false,
         auth: {
-            user: "",
-            pass: "",
-        },
-
-        await transporter.sendMail({
-            from,
-            to,
-            subject,
-            text,
-            from: 'contato <contato@joaquimatheus.site>'
-        })
+            user: process.env.AWS_SES_USER,
+            pass: process.env.AWS_SES_PASSWORD
+        }
     });
+
+    let info = await transporter.sendMail({
+        from: '',
+        to: '',
+        subject: 'Hello =)',
+        text: "llololololololololololo"
+    })
+
+    console.log('Message ID', info.messageId);
 }
+
+sendEmail().catch(console.error);
 
 module.exports = { sendEmail };
