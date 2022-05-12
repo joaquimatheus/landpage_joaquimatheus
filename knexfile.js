f895c4308ea8a522212a588471c79db5544af342
Matheus Zakken <matheus@marcogabriel.com.br>
2022-05-12 19:09:06 -0300

    refactor knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,26 +1,22 @@
 require('./dotenv')
-// Update with your config settings.
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
 
-development: {
-    client: 'pg',
-    connection: {
-        host : process.env.PSQL_DB_HOST,
-        database : process.env.PSQL_DB_DATABASE,
-        user : process.env.PSQL_DB_USERNAME,
-        password : process.env.PSQL_DB_PASSWORD
-    },
-    migrations: {
-        directory: __dirname + '/knex/migrations',
-        tableName: "knex_migrations"
-    },
-    pool : {
-        min: 2,
-        max: 10
-    }
+    development: {
+        client: 'pg',
+        connection: {
+            host : process.env.PSQL_DB_HOST,
+            database : process.env.PSQL_DB_DATABASE,
+            user : process.env.PSQL_DB_USERNAME,
+            password : process.env.PSQL_DB_PASSWORD
+        },
+        migrations: {
+            directory: __dirname + '/knex/migrations',
+            tableName: "knex_migrations"
+        },
+        pool : {
+            min: 0,
+            max: 10
+        }
     }
 }
